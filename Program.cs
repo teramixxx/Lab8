98971f204432a797c04aff83c9300a1e2ac6406f
teramixxx <127118011+teramixxx@users.noreply.github.com>
2023-04-03 14:19:49 +0300

    Decorator

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Decorator.Structural
+{
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+            ConcreteComponent c = new ConcreteComponent();
+            ConcreteDecoratorA d1 = new ConcreteDecoratorA();
+            ConcreteDecoratorB d2 = new ConcreteDecoratorB();
+
+          
+            d1.SetComponent(c);
+            d2.SetComponent(d1);
+
+            d2.Operation();
+
+            Console.ReadKey();
+        }
+    }
+
+
+    public abstract class Component
+    {
+        public abstract void Operation();
+    }
+
+
+    public class ConcreteComponent : Component
+    {
+        public override void Operation()
+        {
+            Console.WriteLine("Погода ясна");
+        }
+    }
+
+
+    public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+
+
+    public class ConcreteDecoratorA : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            Console.WriteLine("Погода пасмурна");
+        }
+    }
+
+
+    public class ConcreteDecoratorB : Decorator
+    {
+        public override void Operation()
+        {
+            base.Operation();
+            AddedBehavior();
+            Console.WriteLine("Падає дощ");
+        }
+
+        void AddedBehavior()
+        {
+        }
+    }
+}
